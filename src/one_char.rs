3311fbb29ca67d3f96a9246b4beda2717a61732c
7sDream <i@7sdre.am>
2019-12-31 03:46:35 +0800

    chore(errormsg): friendly error message when parse char failed

--- src/one_char.rs
+++ src/one_char.rs
@@ -13,22 +13,31 @@ pub struct OneChar(char);
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub enum ParseError {
     EmptyInput,
+    InvalidCodePoint(u32),
     InvalidDigitInRadix(u32),
     UTF8BytesEmpty,
-    UTF8BytesStrLengthNotDividedByTwo,
-    UTF8BytesParseResultHasMultipleChar,
-    InvalidCharacter,
+    UTF8BytesStrCantAlignToBytes,
+    UTF8BytesInvalid,
+    UTF8BytesParseResultMoreThenOneChar,
 }
 
 impl ToString for ParseError {
     fn to_string(&self) -> String {
-        format!("{:?}", self)
+        match self {
+            Self::EmptyInput => "you don't input anything".to_string(),
+            Self::InvalidCodePoint(codepoint) => format!("invalid unicode codepoint {}", codepoint),
+            Self::InvalidDigitInRadix(radix) => format!("has invalid char when parse as a base {} number", radix),
+            Self::UTF8BytesEmpty => "need bytes str after `0x`".to_string(),
+            Self::UTF8BytesStrCantAlignToBytes => "can't align to bytes".to_string(),
+            Self::UTF8BytesInvalid => "invalid utf8 bytes".to_string(),
+            Self::UTF8BytesParseResultMoreThenOneChar => "utf8 bytes represent for more then one char".to_string(),
+        }
     }
 }
 
 impl OneChar {
     pub fn from_codepoint(codepoint: u32) -> Result<Self, ParseError> {
-        char::try_from(codepoint).map(Self).map_err(|_|ParseError::InvalidCharacter)
+        char::try_from(codepoint).map(Self).map_err(|_| ParseError::InvalidCodePoint(codepoint))
     }
 
     pub fn from_codepoint_str_radix(s: &str, radix: u32) -> Result<Self, ParseError> {
@@ -62,7 +71,7 @@ impl OneChar {
                 .map(|two_digits| -> Result<u8, ParseError> {
                     let mut digits = two_digits.chars();
                     let c1 = digits.next().unwrap(); // at least one char because of the `take_while`
-                    let c2 = digits.next().ok_or(ParseError::UTF8BytesStrLengthNotDividedByTwo)?;
+                    let c2 = digits.next().ok_or(ParseError::UTF8BytesStrCantAlignToBytes)?;
                     if c1.is_ascii_hexdigit() && c2.is_ascii_hexdigit() {
                         #[allow(clippy::cast_possible_truncation)] // because two digit hex meets u8 type
                         Ok((c1.to_digit(16).unwrap() * 16 + c2.to_digit(16).unwrap()) as u8)
@@ -71,14 +80,14 @@ impl OneChar {
                     }
                 })
                 .collect::<Result<Vec<_>, _>>()?
-        ).map_err(|_| ParseError::InvalidCharacter)?;
+        ).map_err(|_| ParseError::UTF8BytesInvalid)?;
 
         let mut iter = utf8.chars();
 
         match (iter.next(), iter.next()) {
             (Some(c), None) => Ok(Self(c)),
             (None, None) => Err(ParseError::UTF8BytesEmpty),
-            (Some(_), Some(_)) => Err(ParseError::UTF8BytesParseResultHasMultipleChar),
+            (Some(_), Some(_)) => Err(ParseError::UTF8BytesParseResultMoreThenOneChar),
             // Because an iterator can not produce value after returning None
             (None, Some(_)) => unsafe { unreachable_unchecked() }
         }
